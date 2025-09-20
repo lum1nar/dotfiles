07c004508bbe166c605943ec13cf32a0cfae2938
lum1nar <lum1naxu@gmail.com>
2025-09-20 20:42:56 +0800

    Show LSP detected error and related keymaps Hold the cursor on the error for more than 0.5 secs and it will show the error message
    Add keymap to efficiently locate the error
    <leader>en -> find next error
    <leader>ep -> find previous error

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -10,5 +10,15 @@ local lsp = require('lsp-zero').preset({
 -- (Optional) Configure lua language server for neovim
 lsp.nvim_workspace()
 
+vim.o.updatetime = 500 -- 停留多久觸發 (毫秒)，預設 4000ms 太久了
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focus = false })
+  end
+})
+
+vim.keymap.set("n", "<leader>ep", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "<leader>en", "<cmd>lua vim.diagnostic.goto_next()<CR>", { desc = "Go to next diagnostic message" })
+
 lsp.setup()
 
