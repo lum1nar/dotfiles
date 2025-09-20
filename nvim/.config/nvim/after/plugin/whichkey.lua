5375dcd116fac5fd33e51b4827770d33e750d517
lum1nar <lum1naxu@gmail.com>
2025-09-20 21:33:22 +0800

    Add degub group in which-key

--- nvim/.config/nvim/after/plugin/whichkey.lua
+++ nvim/.config/nvim/after/plugin/whichkey.lua
@@ -2,4 +2,5 @@ local wk = require("which-key")
 wk.add({
   { "<leader>p", group = "Project / File" }, -- group
   { "<leader>z", group = "Zenmode" }, -- group
+  { "<leader>e", group = "Debug" }, -- group
 })
