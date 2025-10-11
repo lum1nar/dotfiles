36b6ad3932bc6d3a1f84d2e3caa0c3e480e41f09
lum1nar <lum1naxu@gmail.com>
2025-10-11 22:20:07 +0800

    Enable mouse support in nvim-hardtime
    By default, nvim-hardtime disables all mouse functionality.
    This change enables mouse support by using the 'disable_mouse' argument,

--- nvim/.config/nvim/after/plugin/hardtime.lua
+++ nvim/.config/nvim/after/plugin/hardtime.lua
@@ -1,6 +1,17 @@
 vim.notify = require("notify"), -- use notify for messages
-require("hardtime").setup()
-    
+
+require("hardtime").setup({
+    -- I need my scrolling back 
+    -- disabled_keys = {
+    --     ["<Up>"] = false, -- Allow <Up> key
+    --     ["<Down>"] = false, -- Allow <Down> key
+    -- },
+    disable_mouse = false -- Enalbe mouse support
+}
+)
+
+
+
 
 
 
