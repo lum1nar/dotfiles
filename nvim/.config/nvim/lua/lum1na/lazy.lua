f2cf74dcef9650d3e3412f98cd602d8503411709
lum1nar <lum1naxu@gmail.com>
2025-11-21 16:21:44 +0800

    rewrite lazy.lua to lazy load time-comsuming plugins
    plugins in question
    
    - lsp-zero (166 ms)
    - nvim-notify (144 ms)
      - nvim-notify was loaded on startup, and tree-sitter.nvim kept calling it for some reason
    - codecompanion (100 ms)
    - live-preview
    - ...
    
    Startup Time after lazy load: 8x ms

--- nvim/.config/nvim/lua/lum1na/lazy.lua
+++ nvim/.config/nvim/lua/lum1na/lazy.lua
@@ -1,56 +1,102 @@
 return require("lazy").setup({
 
+    ---------------------------------------------------------------------------
+    -- Colorscheme
+    ---------------------------------------------------------------------------
+    { -- UI 直接載入
+        "rose-pine/neovim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+            require("lum1na.config.rose-pine")
+        end
+    },
     {
+        "sainnhe/everforest",
+        lazy = true,
+    },
+
+    ---------------------------------------------------------------------------
+    -- Startup screen
+    ---------------------------------------------------------------------------
+    { -- UI 直接載入
+        "goolord/alpha-nvim",
+        lazy = false,
+        config = function()
+            require("lum1na.config.alpha")
+        end
+    },
+
+    ---------------------------------------------------------------------------
+    -- Telescope
+    ---------------------------------------------------------------------------
+    { -- 直接載入
         "nvim-telescope/telescope.nvim",
         tag = "0.1.8",
-        -- or                            , branch = "0.1.x",
-        dependencies = { { "nvim-lua/plenary.nvim" } },
+        dependencies = { "nvim-lua/plenary.nvim" },
+        cmd = "Telescope",
         config = function()
             require("lum1na.config.telescope")
         end
     },
 
-    {
-        "sainnhe/everforest",
-    },
-
-    {
+    ---------------------------------------------------------------------------
+    -- Treesitter
+    ---------------------------------------------------------------------------
+    { -- 開啟檔案再載入
         "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
         run = ":TSUpdate",
         config = function()
             require("lum1na.config.treesitter")
         end
     },
 
-    {
-        "mbbill/undotree",
+    { -- 進入 insert mode 載入
+        "windwp/nvim-ts-autotag",
+        event = "InsertEnter",
+        config = function()
+            require("lum1na.config.nvim-ts-autotag")
+        end
+    },
 
+    ---------------------------------------------------------------------------
+    -- Undo tree
+    ---------------------------------------------------------------------------
+    { -- 負擔小，直接載入
+        "mbbill/undotree",
+        cmd = "UndotreeToggle",
         config = function()
             require("lum1na.config.undotree")
         end
     },
 
-    {
+    ---------------------------------------------------------------------------
+    -- LSP / CMP / Snippets
+    ---------------------------------------------------------------------------
+    { -- 開啟檔案才載入
         "VonHeikemen/lsp-zero.nvim",
         branch = "v1.x",
+
+        event = { "BufReadPre", "BufNewFile" },
+
         dependencies = {
-            -- LSP Support
-            { "williamboman/mason.nvim" },           -- Optional
-            { "williamboman/mason-lspconfig.nvim" }, -- Optional
-            { "WhoIsSethDaniel/mason-tool-installer.nvim" },
-            { "neovim/nvim-lspconfig",                    tag = "v2.3.0" },
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
 
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" },         -- Required
-            { "hrsh7th/cmp-nvim-lsp" },     -- Required
-            { "hrsh7th/cmp-buffer" },       -- Optional
-            { "hrsh7th/cmp-path" },         -- Optional
-            { "saadparwaiz1/cmp_luasnip" }, -- Optional
-            { "hrsh7th/cmp-nvim-lua" },     -- Optional
+            { "neovim/nvim-lspconfig", tag = "v2.3.0" },
 
-            -- Snippets
-            { "L3MON4D3/LuaSnip" },             -- Required
-            { "rafamadriz/friendly-snippets" }, -- Optional
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-nvim-lua",
+
+            "L3MON4D3/LuaSnip",
+            "rafamadriz/friendly-snippets",
         },
 
         config = function()
@@ -59,164 +105,166 @@ return require("lazy").setup({
         end
     },
 
-    {
+    ---------------------------------------------------------------------------
+    -- Copilot
+    ---------------------------------------------------------------------------
+    { -- Insert Mode 才載入
         "github/copilot.vim",
-        tag = "v1.55.0",
-
+        event = "InsertEnter",
         config = function()
             require("lum1na.config.copilot")
         end
     },
 
-    { "ThePrimeagen/vim-be-good" },
+    ---------------------------------------------------------------------------
+    -- UI / UX plugins
+    ---------------------------------------------------------------------------
+    { -- 開啟 vim 後載入
+        "rcarriga/nvim-notify",
+        event = "VeryLazy",
+        config = function()
+            require("lum1na.config.nvim-notify")
+        end
+    },
 
-    {
+    { -- 開啟 vim 後載入
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("lum1na.config.whichkey")
+        end
+    },
+
+    { -- 直接載入
         "folke/zen-mode.nvim",
+        cmd = "ZenMode",
         config = function()
             require("lum1na.config.zenmode")
         end
     },
 
     {
-        "m4xshen/autoclose.nvim",
-        config = function()
-            require("lum1na.config.autoclose")
-        end
-    },
-
-    {
-        "CRAG666/code_runner.nvim",
-        config = function()
-            require("lum1na.config.code_runner")
-        end
-
-    },
-
-    {
-        "goolord/alpha-nvim",
-        lazy = false,
-        config = function()
-            require("lum1na.config.alpha")
-        end
-    },
-
-    {
-        "m4xshen/hardtime.nvim",
-        config = function()
-            require("lum1na.config.hardtime")
-        end
-    },
-
-    {
-        "rcarriga/nvim-notify",
-        config = function()
-            require("lum1na.config.nvim-notify")
-        end
-    },
-
-    {
-        "folke/which-key.nvim",
-        lazy = false,
-        config = function()
-            require("lum1na.config.whichkey")
-        end
-    },
-
-    {
-        "rose-pine/neovim",
-        config = function()
-            vim.cmd("colorscheme rose-pine")
-            require("lum1na.config.rose-pine")
-        end
-    },
-
-    {
-        "olimorris/codecompanion.nvim",
-        tag = "v17.26.0",
-        config = function()
-            require("lum1na.config.codecompanion")
-            require("lum1na.config.fidget-companioncode")
-        end
-    },
-
-    {
-        "MeanderingProgrammer/render-markdown.nvim",
-
-        config = function()
-            require("lum1na.config.render-markdown")
-        end
-    },
-
-    {
         "sphamba/smear-cursor.nvim",
+        event = "VeryLazy",
         config = function()
             require("lum1na.config.smear-cursor")
         end
     },
 
-    {
+    { -- 視窗滑動才載入
         "karb94/neoscroll.nvim",
+        event = "WinScrolled",
         config = function()
             require("lum1na.config.neoscroll")
         end
     },
 
-    { "ravitemer/codecompanion-history.nvim" },
-
-    { "nvimtools/none-ls.nvim" },
-
-    {
-        "windwp/nvim-ts-autotag",
+    ---------------------------------------------------------------------------
+    -- Code Runner
+    ---------------------------------------------------------------------------
+    { -- 負擔小，直接載入
+        "CRAG666/code_runner.nvim",
+        cmd = { "RunCode", "RunFile", "RunProject" },
         config = function()
-            require("lum1na.config.nvim-ts-autotag")
+            require("lum1na.config.code_runner")
         end
     },
 
-    {
-        "nvim-tree/nvim-tree.lua",
-        dependencies = {
-            { "nvim-tree/nvim-web-devicons" },
+    ---------------------------------------------------------------------------
+    -- Markdown Rendering
+    ---------------------------------------------------------------------------
+    { -- 打開 markdown 才載入
+        "MeanderingProgrammer/render-markdown.nvim",
+        ft = { "markdown", "md" },
+        config = function()
+            require("lum1na.config.render-markdown")
+        end
+    },
+
+    ---------------------------------------------------------------------------
+    -- CodeCompanion + History
+    ---------------------------------------------------------------------------
+    { -- 指令輸入才載入
+        "olimorris/codecompanion.nvim",
+        cmd = {
+            "CodeCompanion",
+            "CodeCompanionChat",
+            "CodeCompanionToggle",
+            "CodeCompanionAsk",
         },
+        config = function()
+            require("lum1na.config.codecompanion")
+            require("lum1na.config.fidget-companioncode")
+        end,
+    },
+
+    { "ravitemer/codecompanion-history.nvim", event = "VeryLazy" },
+
+    ---------------------------------------------------------------------------
+    -- File explorers
+    ---------------------------------------------------------------------------
+    { -- 直接載入
+        "nvim-tree/nvim-tree.lua",
+        cmd = "NvimTreeToggle",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
             require("lum1na.config.nvim-tree")
         end
     },
 
-    {
+    { -- 直接載入
         "stevearc/oil.nvim",
+        cmd = "Oil",
         config = function()
             require("lum1na.config.oil")
         end
-
     },
 
-    {
+    ---------------------------------------------------------------------------
+    -- Misc
+    ---------------------------------------------------------------------------
+    { -- 進入 Insertmode 載入
+        "m4xshen/autoclose.nvim",
+        event = "InsertEnter",
+        config = function()
+            require("lum1na.config.autoclose")
+        end
+    },
+
+    { "ThePrimeagen/vim-be-good",             cmd = "VimBeGood" },
+
+    { -- 直接載入
+        "voldikss/vim-floaterm",
+        cmd = "FloatermToggle",
+        config = function()
+            require("lum1na.config.vim-floaterm")
+        end
+    },
+
+    { -- 指令載入
         'brianhuster/live-preview.nvim',
+        cmd = { "LivePreviewOpen", "LivePreviewStart" },
         config = function()
             require('lum1na.config.live-preview')
         end
     },
 
-    {
-        'voldikss/vim-floaterm',
-        config = function()
-            require('lum1na.config.vim-floaterm')
-        end
-    },
+    { "nvimtools/none-ls.nvim", event = "BufReadPre" },
 
-    {
+    ---------------------------------------------------------------------------
+    -- Color picker
+    ---------------------------------------------------------------------------
+    { -- vim 開啟後載入
         "eero-lehtinen/oklch-color-picker.nvim",
         event = "VeryLazy",
-        version = "*",
         keys = {
-            -- One handed keymap recommended, you will be using the mouse
             {
                 "<leader>v",
                 function() require("oklch-color-picker").pick_under_cursor() end,
                 desc = "Color pick under cursor",
             },
         },
-        ---@type oklch.Opts
         opts = {},
-    }
+    },
+
 })
