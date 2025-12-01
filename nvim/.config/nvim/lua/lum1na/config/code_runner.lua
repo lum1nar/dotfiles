86aaed96ac19d89bb160617826ac2834dd6313c7
lum1nar <lum1naxu@gmail.com>
2025-12-01 16:02:44 +0800

    feat(nvim): run code in a new float term

--- nvim/.config/nvim/lua/lum1na/config/code_runner.lua
+++ nvim/.config/nvim/lua/lum1na/config/code_runner.lua
@@ -1,4 +1,23 @@
 require('code_runner').setup({
+    mode = "float",
+    float = {
+        close_key = "<ESC>",
+        -- Window border (see ':h nvim_open_win')
+        border = "rounded",
+
+        -- Num from `0 - 1` for measurements
+        height = 0.8,
+        width = 0.8,
+        x = 0.5,
+        y = 0.5,
+
+        -- Highlight group for floating window/border (see ':h winhl')
+        border_hl = "FloatBorder",
+        float_hl = "Normal",
+
+        -- Transparency (see ':h winblend')
+        blend = 0,
+    },
     filetype = {
         java = {
             "cd $dir &&",
