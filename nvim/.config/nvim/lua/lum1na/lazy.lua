baf71e7c28f139cb7cf7000dcd0d707a8e3a84d6
lum1nar <lum1naxu@gmail.com>
2026-02-26 17:18:18 +0800

    chore: move peep.nvim to local development mode
    - Remove peep.nvim from lazy-lock.json
    - Add dev = true to peep.nvim plugin configuration
    - Add global dev path configuration for local plugin development
    - Clean up commented-out local plugin configuration

--- nvim/.config/nvim/lua/lum1na/lazy.lua
+++ nvim/.config/nvim/lua/lum1na/lazy.lua
@@ -303,34 +303,10 @@ return require("lazy").setup({
         end,
 
     },
-    -- {
-    --     dir = "~/nvim-plugin/peep.nvim/",
-    --     name = "Peep",
-    --     config = function()
-    --         require("peep").setup({
-    --             colors = {
-    --                 label_main = { fg = "#A72703", bg = "#FCB53B", },
-    --                 label_sub = { fg = "#FCB53B", bg = "#44415a", },
-    --                 line_aux = { fg = "#9893a5", },
-    --                 line_preview = { fg = "#7aa2f7" }
-    --             },
-    --
-    --             peep = {
-    --                 duration = 700,
-    --                 column = false,
-    --                 auxline_icon = "·",
-    --                 key_trigger = true,
-    --                 trigger_keys = { "y", "d", "c", "v", "V" },
-    --                 line_preview = true
-    --             }
-    --         })
-    --         vim.keymap.set({ "n", "v" }, "<leader><leader>", function() require("peep").peep() end, { desc = "Peep" })
-    --     end
-    --
-    -- },
 
     {
         "lum1nar/peep.nvim",
+        dev = true,
         config = function()
             require("peep").setup({
                 colors = {
@@ -352,6 +328,7 @@ return require("lazy").setup({
             vim.keymap.set({ "n", "v" }, "<leader><leader>", function() require("peep").peep() end, { desc = "Peep" })
         end
     },
+
     {
         "nvim-treesitter/nvim-treesitter-textobjects",
         branch = "main",
@@ -370,5 +347,10 @@ return require("lazy").setup({
             require("lum1na.config.nvim-treesitter-textobjects")
         end,
     }
+
+}, {
+    dev = {
+        path = "~/nvim-plugin",
+    },
 
 })
