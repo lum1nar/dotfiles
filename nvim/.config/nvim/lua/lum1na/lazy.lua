a7334b36905a1105ca6613501b82ceefe8162a72
lum1nar <lum1naxu@gmail.com>
2026-03-05 15:24:45 +0800

    feat: add markdown plugins and improve UI settings
    - Add markdown-preview.nvim for live markdown preview
    - Add markview.nvim for markdown file management
    - Disable transparency in rose-pine theme to fix markview hightlight
      group issue
    - Enable cursor line highlighting for better visual feedback

--- nvim/.config/nvim/lua/lum1na/lazy.lua
+++ nvim/.config/nvim/lua/lum1na/lazy.lua
@@ -346,7 +346,23 @@ return require("lazy").setup({
         config = function()
             require("lum1na.config.nvim-treesitter-textobjects")
         end,
-    }
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
+    {
+        "OXY2DEV/markview.nvim",
+        lazy = false,
+
+        -- Completion for `blink.cmp`
+        -- dependencies = { "saghen/blink.cmp" },
+    },
 
 }, {
     dev = {
--- nvim/.config/nvim/lua/lum1na/set.lua
+++ nvim/.config/nvim/lua/lum1na/set.lua
@@ -8,6 +8,8 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.cursorline = true
+
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
