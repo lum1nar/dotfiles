e30fd34ea4d719adb3eb1f35cc7bf664ef94e149
lum1nar <lum1naxu@gmail.com>
2026-03-09 19:20:58 +0800

    refactor(config): update keybindings and improve configs
    - Change niri workspace keybindings from spawn commands to focus-workspace
    - Disable neoscroll mappings for <C-y> and <C-e> in Neovim
    - Reorganize tmux config: move history-limit setting, clean up formatting,
      and adjust status bar styling

--- nvim/.config/nvim/lua/lum1na/config/neoscroll.lua
+++ nvim/.config/nvim/lua/lum1na/config/neoscroll.lua
@@ -1,7 +1,7 @@
 require('neoscroll').setup({
     mappings = { -- Keys to be mapped to their corresponding default scrolling animation
         '<C-u>', '<C-d>',
-        '<C-y>', '<C-e>',
+        -- '<C-y>', '<C-e>',
         'zt', 'zz', 'zb',
     },
     hide_cursor = true,          -- Hide cursor while scrolling
