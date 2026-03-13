e045606441e4d262a09ec99b52ecae53deaf3859
Lum1nar <lum1naxu@gmail.com>
2026-03-13 17:45:13 +0800

    feat(fish): Remove time from the right prompt
    Since execution time is already shown, there is no reason to show the
    time anymore

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -32,7 +32,4 @@ for line in (cat ~/.env)
 end
 
 set SPACEFISH_PROMPT_ORDER user dir host git package node docker ruby golang php rust haskell julia aws conda pyenv kubecontext exec_time line_sep battery jobs exit_code char
-set SPACEFISH_RPROMPT_ORDER time
-set SPACEFISH_TIME_SHOW true
-set SPACEFISH_TIME_PREFIX
 set SPACEFISH_DIR_TRUNC 0
