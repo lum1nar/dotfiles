e30e021f70ea9f96bb6b931b08323576aa3a8a17
Lum1nar <lum1naxu@gmail.com>
2026-03-21 16:14:22 +0800

    feat(wezterm): add initial configuration file
    Add a new Wezterm configuration file with custom settings including:
    - Initial window size (120x28)
    - Font (0xProto Nerd Font) and color scheme (rose-pine-moon)
    - Window opacity (80%)
    - Disabled tab bar
    - High FPS settings (144 FPS)
    - WebGpu front end
    - Custom keybindings for copy/paste
    - Increased scrollback buffer (50k lines)

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,43 @@
+-- Pull in the wezterm API
+
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Basic settings
+config.initial_cols = 120
+config.initial_rows = 28
+config.font_size = 12
+config.color_scheme = "rose-pine-moon"
+config.font = wezterm.font("0xProto Nerd Font")
+
+config.window_background_opacity = 0.8
+
+config.underline_thickness = "1.5pt"
+
+config.enable_tab_bar = false
+
+config.max_fps = 144
+config.animation_fps = 144
+config.cursor_blink_ease_in = "EaseOut"
+config.cursor_blink_ease_out = "EaseOut"
+config.default_cursor_style = "BlinkingBlock"
+config.cursor_blink_rate = 650
+
+config.front_end = "WebGpu"
+
+config.keys = {
+	{ key = "c", mods = "CTRL|SHIFT", action = act.CopyTo("Clipboard") },
+	{ key = "v", mods = "CTRL|SHIFT", action = act.PasteFrom("Clipboard") },
+}
+
+-- config.tab_bar_at_bottom = true
+-- config.enable_mouse = true
+config.scrollback_lines = 50000
+
+-- Keybindings (tmux-style)
+
+-- Finally, return the configuration to wezterm:
+return config
