c0210d48c31e06718f5949da4bbef259a5b74a82
lum1nar <lum1naxu@gmail.com>
2026-05-10 00:42:23 +0800

    feat(markdown): improve block styling for headings and code
    - Set heading width to block with custom padding
    - Set code block width to block with minimum width and padding
    - Apply styles to `render-markdown.nvim`

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -12,8 +12,17 @@ return {
 		ft = { "markdown", "codecompanion" },
 		"MeanderingProgrammer/render-markdown.nvim",
 		opts = {
+			heading = {
+				width = "block",
+				left_pad = 2,
+				right_pad = 4,
+			},
 			code = {
 				disable_background = {},
+				width = "block",
+				min_width = 45,
+				left_pad = 2,
+				language_pad = 2,
 			},
 		},
 	},
