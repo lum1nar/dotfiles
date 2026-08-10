0b99678d6b15da275bea2ce23a7646fa8f1d130d
lum1nar <lum1naxu@gmail.com>
2026-08-10 15:18:35 +0800

    feat(fish): add line-numbered search to note picker
    Improves the note function to search file contents with ripgrep, showing previews via bat, and open the selected match at its line in nvim.

--- fish/.config/fish/functions/note.fish
+++ fish/.config/fish/functions/note.fish
@@ -1,9 +1,20 @@
 function note
-    set note (find ~/obsidian/4\ -\ 筆記/ | fzf)
+    set dir ~/obsidian/4\ -\ 筆記/
 
-    if test -z "$note"
+    set result (rg --line-number --no-heading --color=never . "$dir" |
+        fzf \
+            --delimiter ':' \
+            --preview 'bat --color=always --style=numbers --highlight-line {2} {1}' \
+            --preview-window 'right:60%')
+
+    if test -z "$result"
         return
     end
 
-    vim "$note"
+    echo "$result"
+
+    set file (string split ':' "$result")[1]
+    set line (string split ':' "$result")[2]
+
+    nvim "+$line" "$file"
 end
